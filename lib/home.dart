4cb97cdde1ed2e6c9d338a4330d3ef0421e025e0
Dmitriy Kovalenko <runningmaster@gmail.com>
2019-04-24 19:41:52 +0300

    add MyTabbedPage() for test

--- lib/home.dart
+++ lib/home.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: _MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyTabbedPage(), //_MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -98,3 +98,282 @@ class _MyHomePageState extends State<_MyHomePage> {
           ],
         ),
       ),*/
+
+class MyTabbedPage extends StatefulWidget {
+  const MyTabbedPage({Key key}) : super(key: key);
+  @override
+  _MyTabbedPageState createState() => _MyTabbedPageState();
+}
+
+class _MyTabbedPageState extends State<MyTabbedPage>
+    with SingleTickerProviderStateMixin {
+  final List<Tab> myTabs = <Tab>[
+    Tab(text: '1'),
+    Tab(text: '2'),
+    Tab(text: '3'),
+    Tab(text: '4'),
+    Tab(text: '5'),
+    Tab(text: '6'),
+    Tab(text: '7'),
+    Tab(text: '8'),
+    Tab(text: '9'),
+    Tab(text: '10'),
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: myTabs.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(
+          isScrollable: true,
+          controller: _tabController,
+          tabs: myTabs,
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: myTabs.map((Tab tab) {
+          return TabBarViewItem(tab.text, _tabController);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class TabBarViewItem extends StatelessWidget {
+  final String _text;
+  final TabController _tabController;
+
+  TabBarViewItem(this._text, this._tabController);
+
+  @override
+  Widget build(BuildContext context) {
+    //if (_tabController.indexIsChanging) {
+    print(_tabController.index);
+    //}
+    return Center(child: Text(_text));
+  }
+}
+
+class TabBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(text: '1'),
+                Tab(text: '2'),
+                Tab(text: '3'),
+                Tab(text: '4'),
+                Tab(text: '5'),
+              ],
+            ),
+            title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              TabBarViewItem1(),
+              TabBarViewItem2(),
+              TabBarViewItem3(),
+              TabBarViewItem4(),
+              TabBarViewItem5(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TabBarViewItem1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("TabBarViewItem1");
+    //return Icon(Icons.directions_car);
+    return ListView(
+      children: ListTile.divideTiles(
+        context: context,
+        tiles: [
+          ListTile(
+            title: Text('Ingredient 1'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 1'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 2'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 2'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 3'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 3'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 4'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 4'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 5'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 5'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 6'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 6'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 7'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 7'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 7'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 7'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 7'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 7'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 7'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 7'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 7'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 7'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 7'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 7'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 7'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 7'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 7'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 7'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 9'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 7'))),
+          ),
+          ListTile(
+            title: Text('Ingredient 10'),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        TestOne('Ingredient 7'))),
+          ),
+        ],
+      ).toList(),
+    );
+  }
+}
+
+class TabBarViewItem2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("TabBarViewItem2");
+    return Icon(Icons.directions_transit);
+  }
+}
+
+class TabBarViewItem3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("TabBarViewItem3");
+    return Icon(Icons.directions_bike);
+  }
+}
+
+class TabBarViewItem4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("TabBarViewItem4");
+    return Icon(Icons.directions_boat);
+  }
+}
+
+class TabBarViewItem5 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("TabBarViewItem5");
+    return Icon(Icons.directions_railway);
+  }
+}
